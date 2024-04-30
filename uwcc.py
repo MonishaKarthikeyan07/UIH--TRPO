42b93be8f4e874bd8e91b8a4f7d061a6620c4062
Monisha <129486568+MonishaKarthikeyan07@users.noreply.github.com>
2024-04-30 10:44:35 +0530

    uwcc.py

--- uwcc.py
+++ uwcc.py
@@ -1,21 +1,20 @@
 import torch.utils.data as data
 import os
 from PIL import Image
-import numpy as np
 from torchvision import transforms
 
 def img_loader(path):
     img = Image.open(path)
     return img
 
-def get_imgs_list(ori_dirs,ucc_dirs):
+def get_imgs_list(ori_dirs, ucc_dirs):
     img_list = []
     for ori_imgdir in ori_dirs:
-        img_name = (ori_imgdir.split('/')[-1]).split('.')[0]
-        ucc_imgdir = os.path.dirname(ucc_dirs[0])+'/'+img_name+'.png'
+        img_name = os.path.splitext(os.path.basename(ori_imgdir))[0]
+        ucc_imgdir = os.path.join(os.path.dirname(ucc_dirs[0]), img_name + '.png')
 
         if ucc_imgdir in ucc_dirs:
-            img_list.append(tuple([ori_imgdir,ucc_imgdir]))
+            img_list.append((ori_imgdir, ucc_imgdir))
 
     return img_list
 
@@ -25,25 +24,22 @@ class uwcc(data.Dataset):
 
         self.img_list = get_imgs_list(ori_dirs, ucc_dirs)
         if len(self.img_list) == 0:
-            raise(RuntimeError('Found 0 image pairs in given directories.'))
+            raise RuntimeError('Found 0 image pairs in given directories.')
 
         self.train = train
         self.loader = loader
 
-        if self.train == True:
+        if self.train:
             print('Found {} pairs of training images'.format(len(self.img_list)))
         else:
             print('Found {} pairs of testing images'.format(len(self.img_list)))
-            
+
     def __getitem__(self, index):
         img_paths = self.img_list[index]
         sample = [self.loader(img_paths[i]) for i in range(len(img_paths))]
 
-        if self.train == True:
+        if self.train:
             oritransform = transforms.Compose([
-                # transforms.RandomResizedCrop(256,scale=(0.5,1.0)),
-                # transforms.RandomHorizontalFlip(),
-                # transforms.RandomVerticalFlip(),
                 transforms.ToTensor(),
             ])
             ucctransform = transforms.Compose([
